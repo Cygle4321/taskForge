a499933189d604b001846ae185bcc43970bb3f17
Mohamed <mom413760@gmail.com>
2026-06-19 11:52:44 +0100

    Implementation du jwt, Inscription, connexion,logout, et profil implementé aussi la migration pour les todo et leur categoris a deja eté fais sans oublier leur model respectif et dans User aussi hasmany et belongTo

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'Utilisateur créé avec succès !',
+            'user' => $user
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['message' => 'Identifiants incorrects'], 401);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth('api')->user());
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json([
+            'message' => 'Déconnexion réussie'
+        ]);
+    }
+}
